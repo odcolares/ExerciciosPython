1f52a79a3f3755bda1085df2b9de597f570927d4
odcolares <odaircolares@msn.com>
2020-09-23 00:21:38 -0400

    Novos exercicios

--- ex052.py
+++ ex052.py
@@ -2,8 +2,9 @@ from time import sleep
 print('\033[32mMostre o primeiro termo e a razao de uma PA:\033[m ')
 primeiro = int(input('Digite o 1 termo: '))
 razao = int(input('Digite a razao: '))
+print('Vamos calcular!')
 decimo = primeiro + (11 - 1) * razao
 for c in range(primeiro, decimo, razao):
     sleep(.5)
     print('{:2}...'.format(c, end='* '))
-print('ACABOU!!!!')
+print('\033[31mACABOU!!!!')
